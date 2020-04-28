2cc5b395c69d8cb1305d36076e78bfeca9c80a5c
Grisha <grisatkac@mail.ru>
2020-04-28 19:48:49 +0300

    add sm chs and scroll

--- js/index.js
+++ js/index.js
@@ -69,7 +69,6 @@ const initializeSlick = () => {
               settings: "unslick"
           }
       ]
-      
   });
 
   $(".second-slider").slick({
@@ -101,262 +100,66 @@ $(window).on("resize", () => {
   }
 });
 
-
-
 $(document).ready(() => {
   initializeSlick();
 });
 
-
 const mobileMenu = document.querySelector('.mobile-menu-button');
 const menu = document.querySelector('.mobile-menu');
-let isOpened = false;
+const header = document.querySelector('.header');
+let menuOpened = false;
+let previousPosition = 0;
 
 mobileMenu.addEventListener('click', () => {
-    if (isOpened) {
+    if (menuOpened) {
         menu.style.transform = 'translateY(-101%)';
-        isOpened = false;
+        menuOpened = false;
     }
     else {
         menu.style.transform = 'translateY(-1%)';
-        isOpened = true;
+        menuOpened = true;
     }
 });
 
-/* slider */
-/* -1- -2- -3- */
+window.addEventListener("scroll", () => {
+        const currentScrollPosition = pageYOffset + document.documentElement.clientHeight;
+        if (document.documentElement.clientWidth < 1025) return;
+        if (pageYOffset < 70) {
+            header.classList.remove("scroll-down");
+            header.style.boxShadow = 'none';
+        }
+        else {
+            if (currentScrollPosition > previousPosition) {
+                header.classList.add("scroll-down");
+                header.style.boxShadow = '0px 2px 4px rgba(0, 0, 0, 0.12)';
+            }
+            else if (currentScrollPosition < previousPosition) {
+                header.classList.remove("scroll-down");
+                header.style.boxShadow = '0px 2px 4px rgba(0, 0, 0, 0.12)';
+            }
+            previousPosition = currentScrollPosition;
+        }
+});
 
+window.addEventListener("resize", () => {
+        if (document.documentElement.clientWidth < 1000) {
+            header.classList.remove("scroll-down");
+            header.style.boxShadow = 'none';
+        }
+        if (document.documentElement.clientWidth > 1000 && pageYOffset > 70) {
+            header.style.boxShadow = '0px 2px 4px rgba(0, 0, 0, 0.12)';
+        }
+});
 
+const jumpToLink = (link) => {
+    let top = document.getElementsByClassName(link)[0].offsetTop;
 
-/*const container = document.querySelector('.container');*/
-/*const demobtn = document.querySelector('.demo-btn');*/
-/*const widthSlide = document.querySelector(".slide").offsetWidth;
-console.log(document.querySelector(".slide"));
-console.log(document.querySelector(".slide").offsetWidth);*/
+    window.scrollTo(0, top);
+    /*document.querySelector(".mobile-menu-wrap").style.transform = "translateY(-100%)";*/
+    menu.style.transform = "translateY(-101%)";
+    menuOpened = false;
+};
 
-/*demobtn.addEventListener('click',(e) => {
-    e.preventDefault();
-    document.querySelector("#slide2").setAttribute('checked', '');
-});*/
-
-/*const goRight = (currentSlide, targetSlide) => {
-    position -= (targetSlide - currentSlide)*widthSlide;
-    container.style.transform = `translateX(${position}px)`;
-}
-
-const goLeft = (currentSlide, targetSlide) => {
-    position += (currentSlide - targetSlide)*widthSlide;
-    container.style.transform = `translateX(${position}px)`;
-}
-
-
-const controls = document.querySelector('.controls');
-controls.addEventListener('click', (e) => {  
-  if(activeSlide == e.target.value) {      
-      return;
-  } else if (activeSlide < e.target.value ) {
-      goRight(activeSlide, e.target.value);
-      activeSlide = e.target.value;
-
-  } else if (activeSlide > e.target.value ) {
-      goLeft(activeSlide, e.target.value);
-      activeSlide = e.target.value;
-  }
-});*/
-
-
-let activeSlide = 1;
-const controls = document.querySelector('.controls'),
-      controls2 = document.querySelector('.childs').querySelector('.controls');
-let position = 0;
-
-var slider = document.getElementById('slider'),
-    sliderItems = document.getElementById('courses-list'),
-    sliderItems2 = document.getElementById('courses-list2');
-    console.log(sliderItems);
-    console.log(sliderItems2);
-    /*prev = document.getElementById('prev'),
-    next = document.getElementById('next');*/
-    console.log(slider);
-
-function slide(control, items, prev, next) {
-    console.log('function slide working');
-  var posX1 = 0,
-      posX2 = 0,
-      posInitial,
-      posFinal,
-      
-      slides = items.getElementsByClassName('slide1'),
-      slidesLength = slides.length,
-      slideSize = items.getElementsByClassName('slide1')[0].offsetWidth,
-      threshold = slideSize / 2.5,
-      firstSlide = slides[0],
-      lastSlide = slides[slidesLength - 1],
-      cloneFirst = firstSlide.cloneNode(true),
-      cloneLast = lastSlide.cloneNode(true),
-      index = 0,
-      allowShift = true;
-      console.log(items.getElementsByClassName('slide1')[0]);
-
-      console.log(slideSize);
-      console.log(slidesLength);
-      
-  
-  // Clone first and last slide
-  items.appendChild(cloneFirst);
-  items.insertBefore(cloneLast, firstSlide);
-  /*wrapper.classList.add('loaded');*/
-  
-  // Mouse events
-  items.onmousedown = dragStart;
-  
-  // Touch events
-  items.addEventListener('touchstart', dragStart);
-  items.addEventListener('touchend', dragEnd);
-  items.addEventListener('touchmove', dragAction);
-  
-  // Click events
-  /*prev.addEventListener('click', function () { shiftSlide(-1) });
-  next.addEventListener('click', function () { shiftSlide(1) });*/
-  
-  // Transition events
-  items.addEventListener('transitionend', checkIndex);
-  
-  function dragStart (e) {
-    console.log('----------------');
-    console.log('function dragstart working');
-    e = e || window.event;
-    e.preventDefault();
-    posInitial = items.offsetLeft;
-    /*console.log(window.event);*/
-    console.log('start position:',posInitial);
-    
-    if (e.type == 'touchstart') {
-      posX1 = e.touches[0].clientX;
-    } else {
-      posX1 = e.clientX;
-      document.onmouseup = dragEnd;
-      document.onmousemove = dragAction;
-    }
-  }
-
-  function dragAction (e) {
-    console.log('function dragAction working');
-    e = e || window.event;
-    
-    if (e.type == 'touchmove') {
-      /*console.log('posx1:', posX1); 
-      console.log('posx2:', posX2);*/
-      posX2 = posX1 - e.touches[0].clientX;
-      posX1 = e.touches[0].clientX;
-    } else {
-      posX2 = posX1 - e.clientX;
-      posX1 = e.clientX;
-    }
-    items.style.left = (items.offsetLeft - posX2) + "px";
-  }
-  
-  function dragEnd (e) {
-    console.log('function dragEnd working');
-    posFinal = items.offsetLeft;
-    /*console.log('final pos:', posFinal);
-    console.log(posFinal - posInitial);*/
-    if (posFinal - posInitial < -threshold) {
-      shiftSlide(1, 'drag');
-    } else if (posFinal - posInitial > threshold) {
-      shiftSlide(-1, 'drag');
-    } else {
-      items.style.left = (posInitial) + "px";
-    }
-
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-  
-  function shiftSlide(dir, action) {
-    console.log('function shiftSlide working');
-    items.classList.add('shifting');
-    
-    if (allowShift) {
-      if (!action) { posInitial = items.offsetLeft; }
-
-      if (dir >= 1) {
-        items.style.left = (posInitial - slideSize) + "px";
-        index++;      
-      } else if (dir <= -1) {
-        items.style.left = (posInitial + slideSize) + "px";
-        index--;  
-      }
-    };
-    
-    allowShift = false;
-  }
-    
-  function checkIndex (){
-    console.log('function checkIndex working');
-    items.classList.remove('shifting');
-
-    if (index == -1) {
-      items.style.left = -(slidesLength * slideSize) + "px";
-      index = slidesLength - 1;
-    }
-
-    if (index == slidesLength) {
-      items.style.left = -(1 * slideSize) + "px";
-      index = 0;
-    }
-    console.log('index: ', index);
-    
-    allowShift = true;
-  }
-
-  const goRight = (currentSlide, targetSlide) => {
-    console.log(currentSlide, targetSlide);
-    position = -(targetSlide - currentSlide + 1)*slideSize;
-    console.log('position',position);  
-    /*container.style.transform = `translateX(${position}px)`;*/
-    items.style.left = `${position}px`;
-  }
-  
-  const goLeft = (currentSlide, targetSlide) => {
-    position = -(currentSlide - targetSlide +1)*slideSize;
-    /*container.style.transform = `translateX(${position}px)`;*/
-    items.style.left = `${position}px`;
-  }
-
-  control.addEventListener(`${'click' || 'touchstart' }`, (e) => { 
-    let targetPosition = +e.target.value;
-    console.log(targetPosition);
-    console.log('current position: ', index);
-    console.log('target position', e.target.value); 
-    /*if(index == e.target.value) {      
-      console.log('ничего не перемещаем');
-        return;
-    } else if (index < e.target.value ) {
-        console.log('перемещаем вправо');
-        goRight(index, e.target.value);
-        index = e.target.value;
-  
-    } else if (index > e.target.value ) {
-      console.log('перемещаем влево');
-        goLeft(index, e.target.value);
-        index = e.target.value;
-    } else {
-      console.log('error');
-    }*/
-    if(index !== +e.target.value ) {
-      console.log('ok');
-      items.style.left = `${-targetPosition*slideSize}px`;
-      index = e.target.value;
-    }
-  });
-}
-console.log('length doc: ', document.body.offsetWidth);
-/*slide(slider, sliderItems, prev, next);*/
-if ( document.body.offsetWidth < 768 ) {
-  slide(controls,sliderItems );
-  slide(controls2, sliderItems2);
-}
 
 
 
